5e66fb8f4ae3429b7e46adf943e9b65737411ada
Shivam Dixit <shivamd001@gmail.com>
2015-01-31 20:19:49 +0530

    Added yes/no events in flashcard
    Signed-off-by: Shivam Dixit <shivamd001@gmail.com>

--- media/js/flashcard.js
+++ media/js/flashcard.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+
+    var buttonClickedFlag = false;
+
     var flashcardTemplate = '\
     <div class="zingerStage">\
         <div class="zingerFlashcard">\
@@ -8,15 +11,32 @@ $(document).ready(function(){
             <div class="zingerBack">\
                 <p id="zingerCardMeaning"></p>\
                 <p>Did you know this word?</p>\
-                <input type="button" value="Yes"/>\
-                <input type="button" value="No"/>\
+                <input type="button" value="Yes" id="zingerYes"/>\
+                <input type="button" value="No" id="zingerNo"/>\
             </div>\
         </div>\
     </div>';
 
     $('body').prepend(flashcardTemplate);
 
+    $('#zingerYes').click(function(e){
+        buttonClickedFlag = true;
+        alert("Yes");
+        $(".zingerStage").hide();
+    });
+
+    $('#zingerNo').click(function(e){
+        buttonClickedFlag = true;
+        alert("No!");
+        $(".zingerStage").hide();
+        buttonClickedFlag = true;
+    });
+
     $('.zingerFlashcard').on('click', function() {
+        if (buttonClickedFlag) {
+            return;
+        }
+
         $('.zingerFlashcard').toggleClass('zingerFlipped');
     });
 });
