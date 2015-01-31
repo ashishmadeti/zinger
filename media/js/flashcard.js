7e82b2a84642b4c372491e7f9a49eaea323d978a
Shivam Dixit <shivamd001@gmail.com>
2015-01-31 22:21:47 +0530

    Added example/meaning toggle
    Signed-off-by: Shivam Dixit <shivamd001@gmail.com>

--- media/js/flashcard.js
+++ media/js/flashcard.js
@@ -1,5 +1,3 @@
-var buttonClickedFlag = false;
-
 $(document).ready(function(){
     var flashcardTemplate = '\
     <div class="zingerStage">\
@@ -9,6 +7,8 @@ $(document).ready(function(){
             </div>\
             <div class="zingerBack">\
                 <p id="zingerCardMeaning"></p>\
+                <p id="zingerExampleTxt" style="display:none;"></p>\
+                <input type="button" value="Example" id="zingerExampleToggle"/>\
                 <p>Did you know this word?</p>\
                 <input type="button" value="Yes" id="zingerYes"/>\
                 <input type="button" value="No" id="zingerNo"/>\
@@ -28,7 +28,26 @@ $(document).ready(function(){
         $(".zingerStage").hide();
     });
 
-    $('.zingerFlashcard').on('click', function() {
+    $('#zingerExampleToggle').click(function(){
+        if ($('#zingerExampleTxt').is(':hidden')) {
+            // Show example and hide meaning
+            $('#zingerExampleTxt').show();
+            $('#zingerCardMeaning').hide();
+            $('#zingerExampleToggle').val('Meaning');
+        } else {
+           // Show meaning and hide example
+            $('#zingerExampleTxt').hide();
+            $('#zingerCardMeaning').show();
+            $('#zingerExampleToggle').val('Example');
+        }
+     });
+
+    $('.zingerFlashcard').on('click', function(e) {
+        if ($(e.target).is('#zingerExampleToggle')) {
+            // Don't flip card on example button click
+            return;
+        }
+
         $('.zingerFlashcard').toggleClass('zingerFlipped');
     });
 
@@ -38,9 +57,10 @@ $(document).ready(function(){
 // To listen to new word event
 chrome.runtime.onMessage.addListener(function(msg, sender) {
     //use msg.word, msg.meaning and msg.context
-
     $("#zingerCardWord").html(msg.word);
     $("#zingerCardMeaning").html(msg.meaning);
-
+    $("#zingerExampleTxt").html(msg.context);
+    $("#zingerCardMeaning").show(); // If user clicked example previously
+    $("#zingerExampleTxt").hide();  // Don't show example initially
     $(".zingerStage").show();
 });
