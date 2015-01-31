d0452ab95fc29cb80a35b78e7b503c6b1ec28ad3
Shivam Dixit <shivamd001@gmail.com>
2015-02-01 01:51:48 +0530

    Fixed coding conventions
    Signed-off-by: Shivam Dixit <shivamd001@gmail.com>

--- media/js/getContext.js
+++ media/js/getContext.js
@@ -1,23 +1,23 @@
-function getContext(wordSelection){
+function getContext(wordSelection) {
     var wordNode = wordSelection.anchorNode.parentNode;
     var parentNodeText = $(wordNode).parent().text();
     var paragraphText = $(wordNode).text();
     var sentences;
+
     // If only word in the tag like in case of bold
-    if (paragraphText.split(" ").length == 1)
-    {
+    if (paragraphText.split(" ").length == 1) {
         sentences = parentNodeText.split(". ");
-    }
-    else{
+    } else {
         sentences = paragraphText.split(". ");
     }
+
     var context = '';
-    for (var i=0;i<sentences.length;i++)
-    {
+    for (var i=0;i<sentences.length;i++) {
         if (sentences[i].search(word) != -1){
             context = sentences[i];
             break;
         }
     }
+
     return context;
 }
